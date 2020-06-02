0f5f5ad4ef485607de12bb9e9f75340471811c85
Rafael Rey <reyarcenegui@gmail.com>
2020-06-02 15:40:32 +0200

    Added twist mux for integration of joy and nav cmd vels

--- scripts/updateDepends.sh
+++ scripts/updateDepends.sh
@@ -12,7 +12,7 @@ wget https://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
 echo Updating apt cache
 sudo apt-get update
 echo Downloading necessary packages
-sudo apt-get install gazebo9 ros-$ROS_DISTRO-gazebo-ros ros-$ROS_DISTRO-gazebo-plugins gazebo9  ros-$ROS_DISTRO-velodyne-gazebo-plugins -y
+sudo apt-get install gazebo9 ros-$ROS_DISTRO-gazebo-ros ros-$ROS_DISTRO-gazebo-plugins gazebo9  ros-$ROS_DISTRO-velodyne-gazebo-plugins ros-$ROS_DISTRO-twist-mux -y
 
 echo "Nix simulator dependencies installed."
 echo "First, use catkin_make in your catkin workspace."
