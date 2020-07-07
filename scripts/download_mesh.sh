233616cf6c7cec1aa65e863cea1328388b0a8eea
RafaelRey <reyarcenegui@gmail.com>
2020-07-07 16:41:25 +0200

    content corrected

--- scripts/download_mesh.sh
+++ scripts/download_mesh.sh
@@ -1,18 +1,26 @@
 #! /bin/bash
-n="pico_flexx"
 
 # Download the mesh resources:
-for sensor in ${n}_l ${n}_r ${n}_f ${n}_b 
-do
-    echo Adding sensor $sensor
-    mkdir -p models/sensors/$sensor
-    cd models/sensors/$sensor
-    cp -r ../$n/* .
-    sed -i s/$n/$sensor/g model.config
-    sed -i s/$n/$sensor/g $n.sdf
-    mv $n.sdf $sensor.sdf
-    cd ../../..
-done
-    
-echo "Sensors duplicated successfully."
-
+mkdir -p models/mockup_nix/mesh
+cd models/mockup_nix/mesh
+echo "Downloading md5 check file"
+wget --quiet https://www.dropbox.com/s/1gmnnnum9v0qezf/mockup_nix.md5?dl=1 -O mockup_nix.md5
+md5sum --status --check mockup_nix.md5
+Result=$?
+if [[ $Result -eq 0 ]]
+then
+    echo "Nix mesh already downloaded"
+else
+    echo "MD5 check of the mesh failed. Downloading Nix mockup's mesh."
+    wget --quiet https://www.dropbox.com/s/tpcqfmahtj9qxmg/mockup_nix.dae?dl=1 -O mockup_nix.dae
+    echo "Checking MD5 again."
+    ls -l
+    md5sum -c mockup_nix.md5
+    Result=$?
+    if [[ $Result -ne 0 ]]
+    then
+        echo "MD5 error when downloaded the NIX mockup's mesh. Aborting."
+        exit -1
+    fi
+    cd ../../../../../          # Going back to the root of the workspace
+fi
