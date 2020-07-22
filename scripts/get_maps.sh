8b65ef752af2978aba5f868a7a2e9a94ef8f929e
Rafael Rey <reyarcenegui@gmail.com>
2020-07-22 11:17:58 +0200

    fixed multi maps possibility

--- scripts/get_maps.sh
+++ scripts/get_maps.sh
@@ -2,7 +2,7 @@
 #! /bin/bash
 
 # Download the map resources:
-DIR="./resources/maps/sim"
+DIR="./resources/maps/nix_ramp"
 
 if [ -d "$DIR" ]; then
   # Take action if $DIR exists. #
@@ -15,8 +15,8 @@ else
 
   wget --quiet https://www.dropbox.com/sh/t7tf6wjbfzig70q/AAB6C-hW8NtQI-bZiGsQ3ZOKa?dl=1 -O sim.zip
   unzip sim.zip -d nix_ramp/
-  rm sim.zip
   echo "Basic map set succesfully downloaded in the resources/maps folder"
+  cd ../..
 fi
 
 DIR="./resources/maps/nix_ramp_obscure"
@@ -26,10 +26,12 @@ if [ -d "$DIR" ]; then
 else
 
   mkdir -p resources/maps/nix_ramp_obscure
-  cd resources/maps/nix_ramp_obscure
+  cd resources/maps
   echo "Downloading Marsella with tunnel area map"
   wget --quiet https://www.dropbox.com/sh/noqzt6wvvmn20iw/AAB-7nf_PDDiFCs-CYd8ZyD6a?dl=1 -O nix_ramp_obscure.zip
-  unzip nix_ramp_obscure.zip ./
-  rm nix_ramp_obscure.zip
+  unzip nix_ramp_obscure.zip -d nix_ramp_obscure/
   echo "Marsella with tunnel already downloaded"
+  cd ../..
 fi
+
+rm resources/maps/*.zip
