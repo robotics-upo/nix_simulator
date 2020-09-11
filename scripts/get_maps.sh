217a2a2d3e2efa86d1691a857e870b05089614e5
Rafael Rey <reyarcenegui@gmail.com>
2020-09-11 12:31:14 +0200

    Removed --quiet from maps download process

--- scripts/get_maps.sh
+++ scripts/get_maps.sh
@@ -13,7 +13,7 @@ else
   cd resources/maps
   echo "Downloading basic map set"
 
-  wget --quiet https://www.dropbox.com/sh/t7tf6wjbfzig70q/AAB6C-hW8NtQI-bZiGsQ3ZOKa?dl=1 -O sim.zip
+  wget https://www.dropbox.com/sh/t7tf6wjbfzig70q/AAB6C-hW8NtQI-bZiGsQ3ZOKa?dl=1 -O sim.zip
   unzip sim.zip -d nix_ramp/
   echo "Basic map set succesfully downloaded in the resources/maps folder"
   cd ../..
@@ -28,7 +28,7 @@ else
   mkdir -p resources/maps/nix_ramp_obscure
   cd resources/maps
   echo "Downloading Marsella with tunnel area map"
-  wget --quiet https://www.dropbox.com/sh/noqzt6wvvmn20iw/AAB-7nf_PDDiFCs-CYd8ZyD6a?dl=1 -O nix_ramp_obscure.zip
+  wget https://www.dropbox.com/sh/noqzt6wvvmn20iw/AAB-7nf_PDDiFCs-CYd8ZyD6a?dl=1 -O nix_ramp_obscure.zip
   unzip nix_ramp_obscure.zip -d nix_ramp_obscure/
   echo "Marsella with tunnel already downloaded"
   cd ../..
