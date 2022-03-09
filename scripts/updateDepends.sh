142f127ea1810f2110546a3cd0d52b9e6d70e987
david-alejo <churr.te@gmail.com>
2022-03-09 13:14:52 +0100

    Updated configuration procedure. Launch restyle

--- scripts/updateDepends.sh
+++ scripts/updateDepends.sh
@@ -4,17 +4,49 @@ if [[ -z "$ROS_DISTRO" ]]; then
     exit -1
 fi
 
-# Install gazebo 9 last release. 
-echo Adding osrf Repository and downloading the keys
-sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
-wget https://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
-echo Updating apt cache
+# Install our ignition gazebo with radar support
+echo Getting ignition gazebo sources. First dependencies (source )
+sudo apt install python3-pip wget lsb-release gnupg curl
+pip install vcstool || pip3 install vcstool
+pip install -U colcon-common-extensions || pip3 install -U colcon-common-extensions
+# Get vcstool and colcon (the apt way, pip way commented out)
+#pip install vcstool || pip3 install vcstool
+#pip install -U colcon-common-extensions || pip3 install -U colcon-common-extensions
+#export PATH=$PATH:$HOME/.local/bin/
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt-get update
-echo Downloading necessary packages
-sudo apt-get install gazebo9 ros-$ROS_DISTRO-gazebo-ros ros-$ROS_DISTRO-gazebo-plugins gazebo9  ros-$ROS_DISTRO-velodyne-gazebo-plugins ros-$ROS_DISTRO-twist-mux ros-$ROS_DISTRO-teleop-twist-joy ros-$ROS_DISTRO-timed-roslaunch ros-$ROS_DISTRO-octomap ros-$ROS_DISTRO-octomap-ros ros-$ROS_DISTRO-pointcloud-to-laserscan ros-$ROS_DISTRO-ira-laser-tools -y  ros-$ROS_DISTRO-ros-type-instrospection libceres-dev libgsl-dev ros-$ROS_DISTRO-costmap-2d libzmq3-dev libdw-dev ros-$ROS_DISTRO-people-msgs ros-$ROS_DISTRO-map-server ros-$ROS_DISTRO-grid-map-core ros-$ROS_DISTRO-grid-map-ros ros-$ROS_DISTRO-grid-map
+sudo apt-get install python3-vcstool python3-colcon-common-extensions
+sudo apt-get install git
+# Getting the sources
+mkdir -p ~/ign_workspace/src
+cd ~/ign_workspace/src
+wget https://raw.githubusercontent.com/robotics-upo/ign-gazebo/ign-gazebo6/tools/collection-fortress-radar.yaml
+vcs import < collection-fortress-radar.yaml
+#Update dependencies
+sudo wget https://packages.osrfoundation.org/gazebo.gpg -O /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
+sudo apt-get update
+sudo apt -y install \
+  $(sort -u $(find . -iname 'packages-'`lsb_release -cs`'.apt' -o -iname 'packages.apt' | grep -v '/\.git/') | sed '/ignition\|sdf/d' | tr '\n' ' ')
+# Build the ignition workspace
+cd ~/ign_workspace
+colcon graph
+colcon build -DBUILD_TESTING=OFF --merge-install
+
+#Get ign ros
+# Setup the workspace
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
+export IGNITION_VERSION=fortress
+# Download needed software
+git clone https://github.com/osrf/ros_ign.git -b $(ROS_DISTRO)
+cd ~/catkin_ws
+rosdep install -r --from-paths src -i -y --rosdistro $(ROS_DISTRO)
+catkin_make
+source devel/setup.bash
 
 #Downlaod and install kindr library for elevation maps
-
 cd ~/
 git clone https://github.com/anybotics/kindr.git
 cd kindr 
